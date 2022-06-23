9666954a8e8a9d8c6db9b85ccc77cef41d682016
Leonardo Costa <devleonardo27@gmail.com>
2022-06-23 18:42:45 -0300

    Categoria remover foi implementada

--- livraria-front/src/app/components/views/categoria/categoria.service.ts
+++ livraria-front/src/app/components/views/categoria/categoria.service.ts
@@ -1,35 +1,43 @@
-import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-import { environment } from './../../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Categoria } from './categoria.model';
-
+import { MatSnackBar, MatSnackBarModule } from "@angular/material/snack-bar";
+import { environment } from "./../../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Categoria } from "./categoria.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CategoriaService {
-
   baseUrl: String = environment.baseUrl;
 
-  constructor(private http: HttpClient, private _snack: MatSnackBar) { }
+  constructor(private http: HttpClient, private _snack: MatSnackBar) {}
 
-  buscarTodas():Observable<Categoria[]>{
-    const url = `${this.baseUrl}/categorias`
-    return this.http.get<Categoria[]>(url)
+  buscarTodas(): Observable<Categoria[]> {
+    const url = `${this.baseUrl}/categorias`;
+    return this.http.get<Categoria[]>(url);
   }
 
-  salvar(categoria: Categoria):Observable<Categoria>{
-    const url = `${this.baseUrl}/categorias`
-    return this.http.post<Categoria>(url,categoria);
+  buscarPorId(id: String): Observable<Categoria> {
+    const url = `${this.baseUrl}/categorias/${id}`;
+    return this.http.get<Categoria>(url);
   }
 
-  mensagem(mensagemErro: String): void{
-    this._snack.open(`${mensagemErro}`, 'OK', {
-      horizontalPosition: 'end',
-      verticalPosition: 'top',
-      duration: 3000
-    })
+  salvar(categoria: Categoria): Observable<Categoria> {
+    const url = `${this.baseUrl}/categorias`;
+    return this.http.post<Categoria>(url, categoria);
+  }
+
+  remover(id: String): Observable<void> {
+    const url = `${this.baseUrl}/categorias/${id}`;
+    return this.http.delete<void>(url);
+  }
+
+  mensagem(mensagemErro: String): void {
+    this._snack.open(`${mensagemErro}`, "OK", {
+      horizontalPosition: "end",
+      verticalPosition: "top",
+      duration: 3000,
+    });
   }
 }
