ce7943d1644b3c74f833a608c9f4c84b269d0f43
Leonardo Costa <devleonardo27@gmail.com>
2022-07-05 10:49:33 -0300

    tentativa do deploy 2

--- livraria-front/server.js
+++ livraria-front/server.js
@@ -1,15 +1,19 @@
-const express = require('express');
+const express = require("express");
 const path = require('path');
 const app = express();
 
-const appName = 'Livraria-front';
+const PORT = process.env.PORT || 8081;
+
+const appName = 'livraria-front';
 
 app.use(express.static( __dirname + `/dist/${appName}`))
 
 app.get('/*', function ( req, res) {
 
-    res.sendFile(path.join(__dirname, './public', 'Apr29Home.html'));
+  res.sendFile(path.join( __dirname + `/dist/${appName}/index.html`));
 
 });
 
-app.listen(process.env.PORT || 8082)
+app.listen(PORT, () => {
+    console.log('Servidor inicio na porta: ' + PORT)
+})
--- livraria-front/src/environments/environment.prod.ts
+++ livraria-front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,  
-  baseUrl: 'https://v1-app-livraria.herokuapp.com'
+  baseUrl: 'api'
 };
